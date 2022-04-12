ecd36fc8e40d691a5c0089f9830d18794f7e2bc4
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2022-04-12 08:49:00 +0200

    Initial commit

---
+++ src/STS30x.cpp
@@ -0,0 +1,313 @@
+/*    
+ *  The STS30X library is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation, either version 3 of the 
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "STS30x.h"
+
+STS30X::STS30X(const uint8_t address) : _i2c_addr(address) {};
+
+#if defined(__arm__) && defined(TEENSYDUINO)
+void STS30X::begin(i2c_t3& i2cBus, const bool initBus) {
+#else
+void STS30X::begin(TwoWire& i2cBus, const bool initBus) {
+#endif
+  _pI2cBus = &i2cBus;
+  if (initBus) {
+    _pI2cBus->begin();
+  }
+}
+
+uint8_t STS30X::crc8(const uint8_t* data, const size_t len) {
+  // Calculate CRC with the following parameters (datasheet p. 10):
+  // Type: CRC-8
+  // Polynomial: 0x31 (x^8 + x^5 + x^4 + 1)
+  // Init: 0xFF
+  // Final XOR: 0x00
+
+  uint8_t crc = 0xFF; // init to starting value
+  for (size_t i = 0; i < len; i++) {
+    crc ^= data[i];
+    for (uint8_t crcBit = 8; crcBit > 0; crcBit--) {
+      // Apply the polynomial
+      crc = (crc & 0x80) ? (crc << 1) ^ 0x31 : (crc << 1);
+    }
+  }
+  return crc; // No final XOR needed since it is 0x00;
+}
+
+float STS30X::convertToCelsius(const uint16_t value) {
+  return (float)((uint32_t)value * 175) / 65535.0f - 45;
+}
+
+uint16_t STS30X::convertToRaw(const float value) {
+  return (value + 45.0f) * 65535.0f / 175.0f;
+}
+
+bool STS30X::writeCommand(const uint16_t command, const uint8_t* data, const size_t len) {
+  uint8_t cmd[2];
+
+  cmd[0] = command >> 8;
+  cmd[1] = command & 0xFF;
+
+  _pI2cBus->beginTransmission(_i2c_addr);
+  bool result = _pI2cBus->write(cmd, sizeof(cmd)) == sizeof(cmd);
+  if (len) {
+    result = result & (_pI2cBus->write(data, len) == len);
+  }
+  _pI2cBus->endTransmission();
+  return result;
+}
+
+bool STS30X::queryCommand(const uint16_t command, uint8_t* result, const size_t len, const uint16_t delayus) {
+  this->writeCommand(command);
+  delayMicroseconds(delayus);
+
+  _pI2cBus->requestFrom(_i2c_addr, len);
+  size_t numberOfBytesread = _pI2cBus->readBytes(result, len);
+
+  // Check data for validity
+  if ((numberOfBytesread != len) or (result[len-1] != STS30X::crc8(result, len - 1))) {
+    return false;
+  }
+  return true;
+}
+
+void STS30X::stopConversion(void) {
+  this->writeCommand(STS30X::CMD_BREAK);
+  delay(1);
+}
+
+bool STS30X::readStatus(SensorStatus& status) {
+  uint8_t data[3] = {0};
+  bool result = this->queryCommand(STS30X::CMD_READ_STATUS, data, sizeof(data));
+
+  status.checksumError = data[1] &  STS30X::STATUS_CRC_ERROR;
+  status.commandError = data[1] & STS30X::STATUS_COMMAND_ERROR;
+  status.systemReset = data[1] & STS30X::STATUS_SYSTEM_RESET;
+  status.alertTemperature = data[0] & STS30X::STATUS_ALERT_TEMPERATURE;
+  status.heaterEnabled = data[0] & STS30X::STATUS_HEATER;
+  status.alertPending = data[0] & STS30X::STATUS_ALERT_PENDING;
+
+  return result;
+}
+
+void STS30X::clearStatus(void) {
+  this->writeCommand(STS30X::CMD_CLEAR_STATUS);
+  delayMicroseconds(10);
+}
+
+uint32_t STS30X::readSerial(void) {
+  uint8_t data[4] = {0};
+  this->queryCommand(STS30X::CMD_READ_SERIAL, data, sizeof(data), 1000 /*µs*/);
+
+  return data[0] << 24 | data[1] << 16 | data[2] << 8 | data[3] << 0;
+}
+
+// Soft-Resets the sensor. Do make sure to call stopConversion() before calling
+// reset, because the sensor will not reset during conversions.
+void STS30X::reset(void) {
+  this->writeCommand(STS30X::CMD_SOFTRESET);
+  delay(1); // The sensor needs 1 ms max to return to idle (see datasheet p. 3)
+}
+
+void STS30X::setHeaterState(const bool enable) {
+  if (enable) {
+    this->writeCommand(STS30X::CMD_HEATER_ENABLE);
+  } else {
+    this->writeCommand(STS30X::CMD_HEATER_DISABLE);
+  }
+  delayMicroseconds(10);
+}
+
+float STS30X::readTemp(void) {
+  uint16_t value;
+  if (this->readTempRaw(value)) {
+    return STS30X::convertToCelsius(value);
+  } else {
+    return nan("");
+  }
+}
+
+float STS30X::fetchTemp(void) {
+  uint16_t value;
+  if (this->fetchTempRaw(value)) {
+    return STS30X::convertToCelsius(value);
+  } else {
+    return nan("");
+  }
+}
+
+
+uint16_t STS30X::readTempRaw(void) {
+  uint16_t value;
+  this->readTempRaw(value);
+  return value;
+}
+
+uint16_t STS30X::fetchTempRaw(void) {
+  uint16_t value;
+  this->fetchTempRaw(value);
+  return value;
+}
+
+bool STS30X::readTempRaw(uint16_t &value) {
+  uint8_t data[3] = {0};
+  bool result = this->queryCommand(STS30X::CMD_MEASURE_HIGH_REAPEATABILITY, data, sizeof(data), 15000 /*µs*/);
+
+  value = ((uint16_t)data[0] << 8) | data[1] << 0;
+  return result;
+}
+
+bool STS30X::fetchTempRaw(uint16_t &value) {
+  uint8_t data[3] = {0};
+  bool result = this->queryCommand(STS30X::CMD_FETCH_PERIODIC_RESULT, data, sizeof(data));
+
+  value = ((uint16_t)data[0] << 8) | data[1] << 0;
+  return result;
+}
+
+void STS30X::setContinousSampling(const MeasurementsPerSecond mps, const Repeatability rep) {
+  uint16_t cmd = (uint8_t)mps << 8;
+
+  switch (mps) {
+    case MPS_05:
+      switch(rep) {
+        case REP_LOW:
+          cmd |= 0x2F;
+          break;
+        case REP_MEDIUM:
+          cmd |= 0x24;
+          break;
+        case REP_HIGH:
+          cmd |= 0x32;
+          break;
+      }
+      break;
+    case MPS_1:
+      switch(rep) {
+        case REP_LOW:
+          cmd |= 0x2D;
+          break;
+        case REP_MEDIUM:
+          cmd |= 0x26;
+          break;
+        case REP_HIGH:
+          cmd |= 0x30;
+          break;
+      }
+      break;
+    case MPS_2:
+      switch(rep) {
+        case REP_LOW:
+          cmd |= 0x2B;
+          break;
+        case REP_MEDIUM:
+          cmd |= 0x20;
+          break;
+        case REP_HIGH:
+          cmd |= 0x36;
+          break;
+      }
+      break;
+    case MPS_4:
+      switch(rep) {
+        case REP_LOW:
+          cmd |= 0x29;
+          break;
+        case REP_MEDIUM:
+          cmd |= 0x22;
+          break;
+        case REP_HIGH:
+          cmd |= 0x34;
+          break;
+      }
+      break;
+    case MPS_10:
+      switch(rep) {
+        case REP_LOW:
+          cmd |= 0x2A;
+          break;
+        case REP_MEDIUM:
+          cmd |= 0x21;
+          break;
+        case REP_HIGH:
+          cmd |= 0x37;
+          break;
+      }
+      break;
+  }
+  this->writeCommand(cmd);
+}
+
+bool STS30X::readAlertLimitsHigh(uint16_t& set, uint16_t& clear) {
+  uint8_t data[3] = {0};
+
+  bool result = this->queryCommand(STS30X::CMD_READ_ALERT_LIMIT_HIGH_SET, data, sizeof(data));
+  set = ((uint16_t)data[0] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH) | (uint16_t)data[1] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+  if (!result) {
+    return false;
+  }
+
+  result = this->queryCommand(STS30X::CMD_READ_ALERT_LIMIT_HIGH_CLEAR, data, sizeof(data));
+  clear = ((uint16_t)data[0] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH) | (uint16_t)data[1] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+
+  return result;
+}
+
+bool STS30X::setAlertLimitsHigh(const uint16_t set, const uint16_t clear) {
+  uint8_t data[2] = {0};
+
+  data[0] = set >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH;
+  data[1] = set >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+  bool result = this->writeCommand(STS30X::CMD_WRITE_ALERT_LIMIT_HIGH_SET, data, sizeof(data));
+  if (!result) {
+    return false;
+  }
+
+  data[0] = clear >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH;
+  data[1] = clear >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+  result = this->writeCommand(STS30X::CMD_WRITE_ALERT_LIMIT_HIGH_CLEAR, data, sizeof(data));
+
+  return result;
+}
+
+bool STS30X::readAlertLimitsLow(uint16_t& set, uint16_t& clear) {
+  uint8_t data[3] = {0};
+
+  bool result = this->queryCommand(STS30X::CMD_READ_ALERT_LIMIT_LOW_SET, data, sizeof(data));
+  set = ((uint16_t)data[0] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH) | (uint16_t)data[1] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+  if (!result) {
+    return false;
+  }
+
+  result = this->queryCommand(STS30X::CMD_READ_ALERT_LIMIT_LOW_CLEAR, data, sizeof(data));
+  clear = ((uint16_t)data[0] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH) | (uint16_t)data[1] << STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+
+  return result;
+}
+
+bool STS30X::setAlertLimitsLow(const uint16_t set, const uint16_t clear) {
+  uint8_t data[2] = {0};
+
+  data[0] = set >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH;
+  data[1] = set >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+  bool result = this->writeCommand(STS30X::CMD_WRITE_ALERT_LIMIT_LOW_SET, data, sizeof(data));
+  if (!result) {
+    return false;
+  }
+
+  data[0] = clear >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH;
+  data[1] = clear >> STS30X::OFFSET_ALERT_LIMIT_TEMPERATURE_LOW;
+  result = this->writeCommand(STS30X::CMD_WRITE_ALERT_LIMIT_LOW_CLEAR, data, sizeof(data));
+
+  return result;
+}
---
+++ src/STS30x.h
@@ -0,0 +1,116 @@
+/*    
+ *  The STS30x library is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation, either version 3 of the 
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef STS30x_H
+#define STS30x_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#if defined(__arm__) && defined(TEENSYDUINO)
+  #include <i2c_t3.h>
+#else
+  #include <Wire.h>
+#endif
+
+enum MeasurementsPerSecond {
+  MPS_05 = 0x20,
+  MPS_1  = 0x21,
+  MPS_2  = 0x22,
+  MPS_4  = 0x23,
+  MPS_10 = 0x27,
+};
+
+enum Repeatability {
+  REP_LOW,
+  REP_MEDIUM,
+  REP_HIGH,
+};
+
+struct SensorStatus {
+   bool checksumError;
+   bool commandError;
+   bool systemReset;
+   bool alertTemperature;
+   bool alertPending;
+   bool heaterEnabled;
+};
+
+class STS30X {
+  public:
+    STS30X(const uint8_t address=0x4A);
+
+    #if defined(__arm__) && defined(TEENSYDUINO)
+    void begin(i2c_t3& i2cBus, const bool initBus=true);
+    #else
+    void begin(TwoWire& i2cBus, const bool initBus=true);
+    #endif
+    void setHeaterState(const bool enable);
+    void setContinousSampling(const MeasurementsPerSecond mps, const Repeatability rep);
+    bool readStatus(SensorStatus& status);
+    void clearStatus(void);
+    void reset(void);
+    void stopConversion(void);
+    float readTemp(void);
+    float fetchTemp(void);
+    uint16_t readTempRaw(void);
+    uint16_t fetchTempRaw(void);
+    uint32_t readSerial(void);
+    bool readAlertLimitsHigh(uint16_t& set, uint16_t& clear);
+    bool setAlertLimitsHigh(const uint16_t set, const uint16_t clear);
+    bool readAlertLimitsLow(uint16_t& set, uint16_t& clear);
+    bool setAlertLimitsLow(const uint16_t set, const uint16_t clear);
+    static float convertToCelsius(const uint16_t value);
+    static uint16_t convertToRaw(const float value);
+  private:
+    uint8_t _i2c_addr;
+    #if defined(__arm__) && defined(TEENSYDUINO)
+    i2c_t3* _pI2cBus = NULL;
+    #else
+    TwoWire* _pI2cBus = NULL;
+    #endif
+
+
+    static const uint16_t CMD_SOFTRESET = 0x30A2;
+    static const uint16_t CMD_BREAK = 0x3093;
+    static const uint16_t CMD_HEATER_ENABLE = 0x306D;
+    static const uint16_t CMD_HEATER_DISABLE = 0x3066;
+    static const uint16_t CMD_READ_STATUS = 0xF32D;
+    static const uint16_t CMD_CLEAR_STATUS = 0x3041;
+    static const uint16_t CMD_READ_SERIAL = 0x3780;
+    static const uint16_t CMD_MEASURE_HIGH_REAPEATABILITY = 0x2400;
+    static const uint16_t CMD_FETCH_PERIODIC_RESULT = 0xE000;
+    static const uint16_t CMD_READ_ALERT_LIMIT_HIGH_SET = 0xE11F;
+    static const uint16_t CMD_READ_ALERT_LIMIT_HIGH_CLEAR = 0xE114;
+    static const uint16_t CMD_READ_ALERT_LIMIT_LOW_SET = 0xE102;
+    static const uint16_t CMD_READ_ALERT_LIMIT_LOW_CLEAR = 0xE109;
+    static const uint16_t CMD_WRITE_ALERT_LIMIT_HIGH_SET = 0x611D;
+    static const uint16_t CMD_WRITE_ALERT_LIMIT_HIGH_CLEAR = 0x6116;
+    static const uint16_t CMD_WRITE_ALERT_LIMIT_LOW_SET = 0x6100;
+    static const uint16_t CMD_WRITE_ALERT_LIMIT_LOW_CLEAR = 0x610B;
+    static const uint8_t OFFSET_ALERT_LIMIT_TEMPERATURE_LOW = 7;
+    static const uint8_t OFFSET_ALERT_LIMIT_TEMPERATURE_HIGH = OFFSET_ALERT_LIMIT_TEMPERATURE_LOW+8;
+    static const uint8_t STATUS_HEATER = 1 << (13-8);
+    static const uint8_t STATUS_ALERT_PENDING = 1 << (15-8);
+    static const uint8_t STATUS_ALERT_TEMPERATURE = 1 << (10-8);
+    static const uint8_t STATUS_SYSTEM_RESET = 1 << 4;
+    static const uint8_t STATUS_COMMAND_ERROR = 1 << 1;
+    static const uint8_t STATUS_CRC_ERROR = 1 << 0;
+
+    bool writeCommand(const uint16_t command, const uint8_t* data=NULL, const size_t len=0);
+    bool queryCommand(const uint16_t command, uint8_t* result, const size_t len, const uint16_t delayus=0);
+    bool readTempRaw(uint16_t &value);
+    bool fetchTempRaw(uint16_t &value);
+    static uint8_t crc8(const uint8_t* data, const size_t len);
+};
+#endif
